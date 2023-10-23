23a39a367ea4a9b02811b7d7431519689b59c392
Herman Menor <herman.menor@gmail.com>
2023-10-23 14:56:18 +0300

    Add imports

--- src/io/github/hmenorjr/gwt/sample/client/stockwatchergwt.java
+++ src/io/github/hmenorjr/gwt/sample/client/stockwatchergwt.java
@@ -1,19 +1,10 @@
 package io.github.hmenorjr.gwt.sample.client;
 
 import io.github.hmenorjr.gwt.sample.shared.FieldVerifier;
-import com.google.gwt.core.client.EntryPoint;
-import com.google.gwt.core.client.GWT;
-import com.google.gwt.event.dom.client.ClickEvent;
-import com.google.gwt.event.dom.client.ClickHandler;
-import com.google.gwt.event.dom.client.KeyCodes;
-import com.google.gwt.event.dom.client.KeyUpEvent;
-import com.google.gwt.event.dom.client.KeyUpHandler;
-import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.Button;
-import com.google.gwt.user.client.ui.DialogBox;
-import com.google.gwt.user.client.ui.HTML;
+import com.google.gwt.user.client.ui.FlexTable;
+import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.Label;
-import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.VerticalPanel;
 
@@ -21,6 +12,12 @@ import com.google.gwt.user.client.ui.VerticalPanel;
  * Entry point classes define <code>onModuleLoad()</code>.
  */
 public class StockWatcherGwt implements EntryPoint {
+  private VerticalPanel mainPanel = new VerticalPanel();
+  private FlexTable stocksFlexTable = new FlexTable();
+  private HorizontalPanel addPanel = new HorizontalPanel();
+  private TextBox newSymbolTextBox = new TextBox();
+  private Button addStockButton = new Button("Add");
+  private Label lastUpdatedLabel = new Label();
   /**
    * The message displayed to the user when the server cannot be reached or
    * returns an error.
